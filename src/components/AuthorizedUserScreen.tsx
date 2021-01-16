1affc88207e0c94c95ff6ccaee01d15a75c0e6aa
MuellerMarius <marius_m@gmx.net>
2021-01-16 13:33:55 +0100

    chore: cleanup

--- src/components/AuthorizedUserScreen.tsx
+++ src/components/AuthorizedUserScreen.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { Link, Route, Switch } from 'react-router-dom';
 import ProtectedRoute from '../auth/ProtectedRoute';
@@ -8,45 +8,23 @@ import Entries from './Entries';
 import UserProfile from './UserProfile';
 import Statistics from './Statistics';
 import ProfileAvatar from './ProfileAvatar';
+import LanguageSelector from './LanguageSelector';
 
 function AuthorizedUserScreen() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const { t } = useTranslation();
   const { logout } = useAuth0();
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <>
       <div>
         Das ist top <Link to="/pro">Open Entries</Link> |{' '}
         <Link to="/">home</Link> | <Link to="/profile">Private Profile</Link>
+        <LanguageSelector />
         <ProfileAvatar />
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          getContentAnchorEl={null}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
-        </Menu>
       </div>
 
       <div>
+        s
         <Button color="primary" onClick={() => logout()}>
           Logout
         </Button>
