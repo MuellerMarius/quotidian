b100c3fc90285d4ec9b775a66d25cc0ede349280
MuellerMarius <marius_m@gmx.net>
2021-03-29 17:08:41 +0200

    feat: Calendar filtering

--- client/src/components/Calendar/index.tsx
+++ client/src/components/Calendar/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import {
+  Card,
+  FormControl,
+  Icon,
+  InputLabel,
+  ListItemIcon,
+  makeStyles,
+  MenuItem,
+  Select,
+  ListSubheader,
+  ListItemText,
+  ListItem,
+} from '@material-ui/core';
+import FolderOpenIcon from '@material-ui/icons/FolderOpen';
+import SentimentSatisfiedOutlinedIcon from '@material-ui/icons/SentimentSatisfiedOutlined';
 import { useTranslation } from 'react-i18next';
 import { DatePickerCalendar } from 'react-nice-dates';
 import { isSameDay } from 'date-fns';
@@ -13,14 +27,39 @@ const useStyles = makeStyles({
   root: {
     padding: 25,
     height: '73vh',
+    textAlign: 'center',
+  },
+  selectRoot: {
+    minWidth: 175,
+  },
+  menuDivider: {
+    borderColor: 'rgba(0, 0, 0, 0.08)',
+  },
+  listIconCat: {
+    minWidth: 32,
+  },
+  listIconAct: {
+    minWidth: 32,
+    marginLeft: 8,
   },
 });
 
 const Calendar: React.FC<CalendarProps> = ({ activeMonth, setActiveMonth }) => {
-  const { selectedDate, entries, dispatch } = useGlobalContext();
+  const { t } = useTranslation();
   const { i18n } = useTranslation();
-  const classes = useStyles();
+  const { selectedDate, entries, activities, dispatch } = useGlobalContext();
+  const [selectedModifiers, setSelectedModifiers] = React.useState('moods');
   const locale = getDateFnsLocale(i18n.language);
+  const classes = useStyles();
+
+  const modifiersClassNames = {
+    horrible: '-horrible-mood',
+    bad: '-bad-mood',
+    okay: '-okay-mood',
+    good: '-good-mood',
+    super: '-super-mood',
+    marked: '-marked',
+  };
 
   const isMoodOnDate = (date: Date, mood: number) => {
     const entry = entries?.find((elem) => isSameDay(elem.date, date));
@@ -30,11 +69,27 @@ const Calendar: React.FC<CalendarProps> = ({ activeMonth, setActiveMonth }) => {
     return false;
   };
 
+  const isActivityOnDate = (date: Date, actId: string | string[]) => {
+    const entry = entries?.find((elem) => isSameDay(elem.date, date));
+    if (typeof actId === 'string') {
+      if (entry && entry.activities.includes(actId)) {
+        return true;
+      }
+    } else if (entry && entry.activities.some((act) => actId.includes(act))) {
+      return true;
+    }
+    return false;
+  };
+
   const selectDate = (date: Date | null) => {
     dispatch!({ type: ActionNames.SELECT_DATE, payload: { date } });
   };
 
-  const modifiers = {
+  const handleModifiersSelect = (e: React.ChangeEvent<{ value: unknown }>) => {
+    setSelectedModifiers(e.target.value as string);
+  };
+
+  const moodModifiers = {
     horrible: (date: Date) => isMoodOnDate(date, 0),
     bad: (date: Date) => isMoodOnDate(date, 1),
     okay: (date: Date) => isMoodOnDate(date, 2),
@@ -42,26 +97,94 @@ const Calendar: React.FC<CalendarProps> = ({ activeMonth, setActiveMonth }) => {
     super: (date: Date) => isMoodOnDate(date, 4),
   };
 
-  const modifiersClassNames = {
-    horrible: '-horrible-mood',
-    bad: '-bad-mood',
-    okay: '-okay-mood',
-    good: '-good-mood',
-    super: '-super-mood',
+  const getModifiers = (modifier: string) => {
+    if (modifier === 'moods') {
+      return moodModifiers;
+    }
+
+    if (modifier.startsWith('a-')) {
+      // Single activity
+      return {
+        marked: (date: Date) => isActivityOnDate(date, modifier.substring(2)),
+      };
+    }
+
+    if (modifier.startsWith('c-')) {
+      // Activity category (multiple activities)
+      const category = activities.find((c) => c._id === modifier.substring(2));
+      if (category) {
+        return {
+          marked: (date: Date) =>
+            isActivityOnDate(
+              date,
+              category.activities.map((act) => act._id)
+            ),
+        };
+      }
+    }
+
+    return undefined;
   };
 
   return (
-    <div className={classes.root}>
+    <Card className={classes.root} elevation={2}>
+      <FormControl variant="outlined" size="small">
+        <InputLabel id="view-select-label">{t('show')}</InputLabel>
+        <Select
+          labelId="view-select-label"
+          id="view-select"
+          label={t('show')}
+          classes={{ root: classes.selectRoot }}
+          value={selectedModifiers}
+          onChange={handleModifiersSelect}
+        >
+          <MenuItem value="moods">
+            <ListItem component="span" dense disableGutters>
+              <ListItemIcon classes={{ root: classes.listIconCat }}>
+                <SentimentSatisfiedOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText>{t('moods.name')}</ListItemText>
+            </ListItem>
+          </MenuItem>
+
+          {activities.map((category) => [
+            <ListSubheader>
+              <hr className={classes.menuDivider} />
+            </ListSubheader>,
+
+            <MenuItem value={`c-${category._id}`}>
+              <ListItem component="span" dense disableGutters>
+                <ListItemIcon classes={{ root: classes.listIconCat }}>
+                  <FolderOpenIcon />
+                </ListItemIcon>
+                <ListItemText>{category.name}</ListItemText>
+              </ListItem>
+            </MenuItem>,
+
+            category.activities.map((activity) => (
+              <MenuItem value={`a-${activity._id}`}>
+                <ListItem component="span" dense disableGutters>
+                  <ListItemIcon classes={{ root: classes.listIconAct }}>
+                    <Icon>{activity.icon}</Icon>
+                  </ListItemIcon>
+                  <ListItemText>{activity.name}</ListItemText>
+                </ListItem>
+              </MenuItem>
+            )),
+          ])}
+        </Select>
+      </FormControl>
+
       <DatePickerCalendar
         locale={locale}
         date={selectedDate || undefined}
         month={activeMonth}
         onMonthChange={(month) => setActiveMonth(month || new Date())}
         onDateChange={(date) => selectDate(date)}
-        modifiers={modifiers}
+        modifiers={getModifiers(selectedModifiers)}
         modifiersClassNames={modifiersClassNames}
       />
-    </div>
+    </Card>
   );
 };
 
